86a8daf34a674b36066c7ac5f5b6b6e1f3959840
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-04 11:21:24 -0700

    = to <- and deleted un-needed comments.

--- R/distributions.R
+++ R/distributions.R
@@ -23,121 +23,84 @@
 ##' @name Distributions
 NULL
 
-# dPL - probability density function for unbounded power-law distribution
-# pPL - probability distribution function for unbounded power-law distribution
-# rPL - random numbers from an unbounded power-law distribution
-# dPLB - probability density function for bounded power-law distribution
-# pPLB - probability distribution function for bounded power-law distribution
-# rPLB - random numbers from a bounded power-law distribution
-
 ##' @rdname Distributions
 ##' @export
-dPL = function(x = 1, b = -2, xmin = 1)
+dPL <- function(x = 1, b = -2, x_min = 1)
   {
-    if(b >= -1 | xmin <= 0) stop("Parameters out of bounds in dPL")
-    C = - (b+1) / xmin^(b+1)
-    y = 0 * x     # so have zeros where x < xmin
-    y[x >= xmin] = C * x[x >= xmin]^b
+    if(b >= -1 | x_min <= 0) stop("Parameters out of bounds in dPL")
+    C <- - (b+1) / x_min^(b+1)
+    y <- 0 * x     # so have zeros where x < x_min
+    y[x >= x_min] <- C * x[x >= x_min]^b
     return(y)
   }
 
 ##' @rdname Distributions
 ##' @export
-pPL = function(x = 10, b = -2, xmin = 1)
+pPL <- function(x = 10, b = -2, x_min = 1)
   {
-    if(b >= -1 | xmin <= 0) stop("Parameters out of bounds in qPL")
-    y = 0 * x     # so have zeros where x < xmin
-    y[x >= xmin] = 1 - (x[x >= xmin]/xmin)^(b+1)
+    if(b >= -1 | x_min <= 0) stop("Parameters out of bounds in qPL")
+    y <- 0 * x     # so have zeros where x < x_min
+    y[x >= x_min] <- 1 - (x[x >= x_min]/x_min)^(b+1)
     return(y)
   }
 
 ##' @rdname Distributions
 ##' @export
-rPL = function(n = 1, b = -2, xmin = 1)
+rPL <- function(n = 1, b = -2, x_min = 1)
   {
-    if(b >= -1 | xmin <= 0) stop("Parameters out of bounds in rPL")
-    u = runif(n)
-    y = xmin * ( 1 - u ) ^ (1/(b+1))
+    if(b >= -1 | x_min <= 0) stop("Parameters out of bounds in rPL")
+    u <- runif(n)
+    y <- x_min * ( 1 - u ) ^ (1/(b+1))
     return(y)
   }
 
 ##' @rdname Distributions
 ##' @export
-dPLB = function(x = 1, b = -2, xmin = 1, xmax = 100)
+dPLB <- function(x = 1, b = -2, x_min = 1, x_max = 100)
   {
-    if(xmin <= 0 | xmin >= xmax) stop("Parameters out of bounds in dPLB")
+    if(x_min <= 0 | x_min >= x_max) stop("Parameters out of bounds in dPLB")
     if(b != -1)
-        { C = (b+1) / ( xmax^(b+1) - xmin^(b+1) )
+        { C <- (b+1) / ( x_max^(b+1) - x_min^(b+1) )
         } else
-        { C = 1/ ( log(xmax) - log(xmin) )
+        { C <- 1/ ( log(x_max) - log(x_min) )
         }
-    y = 0 * x     # so have zeros where x < xmin or x > xmax
-    y[x >= xmin & x <= xmax] = C * x[x >= xmin & x <= xmax]^b
+    y <- 0 * x     # so have zeros where x < x_min or x > x_max
+    y[x >= x_min & x <= x_max] <- C * x[x >= x_min & x <= x_max]^b
     return(y)
   }
 
 ##' @rdname Distributions
 ##' @export
-pPLB = function(x = 10, b = -2, xmin = 1, xmax = 100)
+pPLB <- function(x = 10, b = -2, x_min = 1, x_max = 100)
   {
-    if(xmin <= 0 | xmin >= xmax) stop("Parameters out of bounds in pPLB")
-    y = 0 * x        # so have zeros where x < xmin
-    y[x > xmax] = 1
+    if(x_min <= 0 | x_min >= x_max) stop("Parameters out of bounds in pPLB")
+    y <- 0 * x        # so have zeros where x < x_min
+    y[x > x_max] <- 1
     if(b != -1)
-        {  xmintobplus1 = xmin^(b+1)
-           denom = xmax^(b+1) - xmintobplus1
-           y[x >= xmin & x <= xmax] =
-               ( x[x >= xmin & x <= xmax]^(b + 1) - xmintobplus1 ) / denom
+        {  x_mintobplus1 <- x_min^(b+1)
+           denom <- x_max^(b+1) - x_mintobplus1
+           y[x >= x_min & x <= x_max] <-
+                                        ( x[x >= x_min & x <= x_max]^(b + 1) -
+                                          x_mintobplus1 ) / denom
         } else
-        {  logxmin = log(xmin)
-           denom = log(xmax) - logxmin
-           y[x >= xmin & x <= xmax] =
-               ( log( x[x >= xmin & x <= xmax] ) - logxmin ) / denom
+        {  logx_min <- log(x_min)
+           denom <- log(x_max) - logx_min
+           y[x >= x_min & x <= x_max] =
+               ( log( x[x >= x_min & x <= x_max] ) - logx_min ) / denom
         }
     return(y)
   }
 
 ##' @rdname Distributions
 ##' @export
-rPLB = function(n = 1, b = -2, xmin = 1, xmax = 100)
+rPLB <- function(n = 1, b = -2, x_min = 1, x_max = 100)
   {
-    if(xmin <= 0 | xmin >= xmax) stop("Parameters out of bounds in rPLB")
-    u = runif(n)
+    if(x_min <= 0 | x_min >= x_max) stop("Parameters out of bounds in rPLB")
+    u <- runif(n)
     if(b != -1)
-        { y = ( u*xmax^(b+1) +  (1-u) * xmin^(b+1) ) ^ (1/(b+1))
+        { y <- ( u*x_max^(b+1) +  (1-u) * x_min^(b+1) ) ^ (1/(b+1))
         } else
-        { y = xmax^u * xmin^(1-u)
+        { y <- x_max^u * x_min^(1-u)
         }
     return(y)
   }
-
-# PLBfunctions.r - functions to be sourced, including density, distribution
-#
-# CONTENTS
-#
-# Statistical functions:
-# negLL.PLB - negative log-likelihood function for PLB model
-# sum.bins - calculate the total sum of values within a bin
-# Llin.method - fitting data using the Llin method
-# LBNbiom.method - fitting data using the LBbiom and LBNbiom methods
-# LBmizbinsFuns - calculate the bin breaks for the LBmiz method from mizer
-# log2bins - construct bins that double in size and encompass the data
-# eightMethods - computes exponents for a dataset using all eight methods, for
-#  a second manuscript
-# negLL.PLB.binned - negative log-likelihood function for PLB when data are
-#  only available in binned form
-#
-# Plotting functions:
-# lm.line - plot straight line of lm fit but restricted to the x values
-# gap.barplot.cust - customised version of Jim Lemon's gap.barplot for
-#  histograms with a break in an axis
-# qqtab - constructs automated LaTeX code for tables of quantiles
-# confPlot - plotting of the confidence intervals for Figure 4
-# histAxes - histogram axes for histogram plots of estimated b values
-#  (Figure 3 and others)
-# histAxes2 - histAxes adapted for fitting3rep-n10000.r, for n=10,000 sample size
-# logTicks - add axes and tick marks to a log-log plot to represent
-#  unlogged values (e.g. Figures 2(h) and 6(b))
-# legJust - add legend to a plot
-#
-#  2nd Sept 2014 onwards.
