a24a213b817536e101aa2177c0a66bc003b39a40
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-04 16:51:55 -0700

    Document negLL.PLB.binned().

--- R/fitting.R
+++ R/fitting.R
@@ -781,27 +781,34 @@ eightMethods = function(oneYear = 1980,
   dev.off()
   return(eightMethodsRes)
 }
-
+##' Calculate negative log-likelihood for the bounded power-law
+##'   distribution given binned data
+##'
+##' Calculates the negative log-likelihood of the parameters `b`, `xmin` and
+##' `xmax` given binned data for the PLB model. Returns the negative
+##' log-likelihood. Will be called by `nlm()` or similar, but `xmin` and `xmax`
+##' will just be estimated as the minimum of lowest bin and maximum of the
+##' largest bin, respectively, (since they are the MLEs), no need to do
+##' numerically. Specifically this is the negative of the log-likelihood
+##' function given in (A.70) of Edwards et al. (2017) and (S.27) of Edwards et
+##' al. (TODO), where the latter fixed a minor error in (A.75) of Edwards et
+##' al. (2017).
+##'
+##' @param b value of b for which to calculate the negative log-likelihood
+##' @param w vector of length `J+1` giving the bin breaks `w_1, w_2, ..., w_{J+1}`
+##' @param d vector of length `J` giving the count in each bin; must have `d_1,
+##'   d_J > 0`
+##' @param J number of bins (length of `d`)
+##' @param xmin minimum value of bins, as an input to avoid repeatedly calculating
+##' @param xmax maximum value of bins, as an input to avoid repeatedly calculating
+##' @return negative log-likelihood of the parameters given the data
+##' @export
+##' @author Andrew Edwards
 negLL.PLB.binned = function(b, w, d, J=length(d), xmin=min(w), xmax=max(w))
                                         # sumlogx)
   {
-  # Calculates the negative log-likelihood of the parameters b, xmin and xmax
-  #  given binned data for the PLB model. Returns the negative log-likelihood.
-  #  Will be called by nlm or similar, but xmin and xmax will just be estimated
-  #  as the min of lowest bin and max of the largest bin (that is their MLEs),
-  #  no need to do numerically. See Appendix for derivation.
-  #
-  # Args:
-  #   b: value of b for which to calculate the negative log-likelihood
-  #   w: vector of length J+1 giving the bin breaks w_1, w_2, ..., w_{J+1}
-  #   d: vector of length J giving the count in each bin. Must have d_1, d_J >0
-  #   J: number of bins (length of d)
-  #   xmin: minimum value of bins, as an input to avoid repeatedly calculating
-  #   xmax: maximum value of bins, as an input to avoid repeatedly calculating
-  #
-  # Returns:
-  #   negative log-likelihood of the parameters given the data.
-  #
+   # TODO fix those J=length(d) type things in args, that messed me up in
+   # another project
     if(xmin <= 0 | xmin >= xmax | length(d) != J | length(w) != J+1 |
          d[1] == 0 | d[J] == 0 | min(d) < 0)
          stop("Parameters out of bounds in negLL.PLB")
