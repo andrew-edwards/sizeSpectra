6e47e2a1a968f2c4bf1be75dbf8834c421719103
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-21 12:14:48 -0700

    IBTS_data (was IBTS.data) is now a tibble and tibbles now get handled correctly since tibble package now imported, see #3.

--- data-raw/IBTS-data.R
+++ data-raw/IBTS-data.R
@@ -1,14 +1,19 @@
 # The full IBTS data set used in the MEPS paper.
 
+# Needs running once to save tibbles as tibbles (it puts tibble in Imports)
+usethis::use_tibble()
+
 load("ibtsQ1cpuelength.RData")    # Original file as downloaded by Julia
                                   # Blanchard. Contains data frame q1.
-dataOrig = dplyr::tbl_df(q1)
+dataOrig = tibble::as_tibble(q1)  # TODO - not run yet with as_tibble
 
 usethis::use_data(dataOrig, overwrite = TRUE)
 
 # See TODO vignette
 load("nSeaFungImport.RData") # TODO prob change me
 
-IBTS.data = dplyr::ungroup(data)   # Else some groups get kept
+data = tibble::as_tibble(data)
 
-usethis::use_data(IBTS.data, overwrite = TRUE)
+IBTS_data = dplyr::ungroup(data)   # Else some groups get kept
+
+usethis::use_data(IBTS_data, overwrite = TRUE)
