fcb88a3b957171f3de23c5c3edc5c9a49995d4e4
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-21 15:04:03 -0700

    use_tibble asked me to do this.

---
+++ R/sizeSpectra-package.R
@@ -0,0 +1,8 @@
+## As instructed from using usethis::use_tibble() [else saved data tibbles don't
+##  get used properly]
+
+
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
