72120ea575358a5fce714c2d0441eaf8c738a423
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-21 16:45:08 -0700

    Save some results.

--- data-raw/IBTS-MLEbins.R
+++ data-raw/IBTS-MLEbins.R
@@ -3,4 +3,20 @@
 
 load("nSeaMLEbins.RData")         # Output from nSeaMLEbins.Snw. TODO
                                   # should then become output from vignette maybe
-usethis::use_data(TODO, overwrite = TRUE)
+usethis::use_data(dataBin, overwrite = TRUE)
+
+# TODO some results also, so someone can compare them
+# Need MLEbins.res, but it's done in vignette. Well, it is saved in
+
+# From the end of MEPS_IBTS_MLEbins.Rmd (since this was originally in
+# nSeaMLEbins-recommend.Snw):
+
+# TODO: double check .Rmd code didn't change since fcb88a3
+MLEbins.res = MLEbins.nSeaFung.new
+MLEbins.res = dplyr::mutate(MLEbins.res,
+                            C = (b != -1 ) * (b+1) / ( xmax^(b+1) - xmin^(b+1) ) +
+                                (b == -1) * 1 / ( log(xmax) - log(xmin) )
+                           )
+MLEbins.res = dplyr::select(MLEbins.res, -stdErr)
+
+usethis::use_data(MLEbins.res, overwrite = TRUE)
