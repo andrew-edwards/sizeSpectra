7c58f99d41e3790465ee4010eb35fad9245b897b
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-21 17:22:23 -0700

    Add Chris's f() formatting function.

--- R/utilities.R
+++ R/utilities.R
@@ -18,3 +18,21 @@ lengthToMass = function(lengths, LWa, LWb)
             { stop("Need positive arguments in lengthToMass")  }
         return(LWa * lengths^LWb)
     }
+
+##' Format x to have the specified number of decimal places
+##'
+##' TODO: change points to places presumably
+##' TODO: give a more descriptive name
+##' Format x to have supplied number of decimal places, have
+##'  thousands seperated by commas.
+##' Taken from our Pacific Hake assessment:
+##' https://github.com/cgrandin/hake-assessment/blob/master/doc/r/r-functions/utilities.r
+##'
+##' @param x TODO
+##' @param dec.points  TODO
+##' @return
+##' @export
+##' @author Chris Grandin
+f <- function(x, dec.points = 0){
+  return(format(round(x,dec.points), big.mark = ",", nsmall = dec.points))
+}
