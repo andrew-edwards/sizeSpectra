cccbc6269627f07ce903b4c1fcca062fe2936f42
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-23 13:39:03 -0700

    Deleted no-longer-needed MLEarray etc.

--- data-raw/simulate-data2.R
+++ data-raw/simulate-data2.R
@@ -7,16 +7,6 @@ MLEbin.MEPS.default <- MLEbin.simulate()
 usethis::use_data(MLEbin.MEPS.default, overwrite = TRUE)
 
 
-# Do default value then do:
-expect_equal(MLEbin.MEPS.default$MLE.array, MLE.array)
-expect_equal(MLEbin.MEPS.default$MLEconf.array, MLEconf.array)
-expect_equal(MLEbin.MEPS.default$MLE.array.parameters, MLE.array.known)  # may
-                                        # not work with name change?
-
-# Can delete these then:
-usethis::use_data(MLE.array, overwrite = TRUE)
-usethis::use_data(MLEconf.array, overwrite = TRUE)
-usethis::use_data(MLE.array.known, overwrite = TRUE)
 
 
 # Then expect to do these:
