6e6fb7278405ab39e37664a832b7cb2f490dc13e
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-11-15 14:00:23 -0800

    Save data objects in vignette, now finalised.

--- data-raw/IBTS-MLEbins.R
+++ data-raw/IBTS-MLEbins.R
@@ -1,26 +1,9 @@
 # The MLEbins analyses of the IBTS data in the MEPS paper.
 
-# dataBin is created (and saved once) in vignettes/MEPS_IBTS_MLEbins.Rmd
+# vignettes/MEPS_IBTS_MLEbins.Rmd saves two calculated objects:
 
-load("nSeaMLEbins.RData")         # Output from nSeaMLEbins.Snw. TODO
-                                  # should then become output from vignette maybe
+# usethis::use_data(dataBin, overwrite = TRUE)
 
+# and then at the end:
 
-
-
-
-# TODO some results also, so someone can compare them
-# Need MLEbins.res, but it's done in vignette. Well, it is saved in
-
-# From the end of MEPS_IBTS_MLEbins.Rmd (since this was originally in
-# nSeaMLEbins-recommend.Snw):
-
-# TODO: double check .Rmd code didn't change since fcb88a3
-MLEbins.res = MLEbins.nSeaFung.new
-MLEbins.res = dplyr::mutate(MLEbins.res,
-                            C = (b != -1 ) * (b+1) / ( xmax^(b+1) - xmin^(b+1) ) +
-                                (b == -1) * 1 / ( log(xmax) - log(xmin) )
-                           )
-MLEbins.res = dplyr::select(MLEbins.res, -stdErr)
-
-usethis::use_data(MLEbins.res, overwrite = TRUE)
+# usethis::use_data(MLEbins.res, overwrite = TRUE)
